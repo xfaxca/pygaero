5b95be3803e81ce40532f9b90873c07906b07ca6
xfaxca <cameron@tutanota.com>
2016-11-06 10:32:57 -0600

    Commit 1
    Added __init__.py and empty module gen_chem.py, which will contain
    general chemistry functions.

---
+++ __init__.py
@@ -0,0 +1,20 @@
+# __init__.py
+
+# Documentation, version #, etc.
+__doc__ = "Package: pygaero v1.0\n" \
+          "Created: 09/02/2016\n" \
+          "Author contact: Cameron@tutanota.com \n" \
+          "Description: Python tools for the processing of data obtained from HR-ToF-CIMS, with some\n" \
+          "functions being specifically designed for data obtained using the Filter Inlet for Gases and\n" \
+          "Aerosols (FigAERO) inlet. Time series data is handled by pandas DataFrames, with data \n" \
+          "imported from csv files. Some functions will work for any generic numerical time series.\n" \
+          " \n"
+__author__ = "Cameron Faxon"
+__copyright__ = "Copyright (C) 2016 Cameron Faxon"
+__license__ = "GNU GPLv3"
+__version__ = "1.0"
+__all__ = []
+
+
+
+
---
+++ gen_chem.py
@@ -0,0 +1,6 @@
+# gen_chem.py
+
+__doc__ = "Module containing general chemistry functions to handle chemical formula names, \n" \
+          "elemental analysis of formulas, etc.\n" \
+          "Functions \n" \
+          "[insert functions]"
